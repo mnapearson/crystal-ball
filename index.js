ef9d0f7d8eb50f30080c8c08256c83f4762670da
mnapearson <marratoon@gmail.com>
2020-09-04 14:05:55 +0200

    add crystal ball to this branch

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const db = {
-  words: [
-    "monkey",
-    "lion",
-    "giraffe",
-    "zebra",
-    "mouse",
-    "beaver",
-    "turtle",
-    "horse",
-    "tiger",
-    "fox",
-    "swan",
-    "gorilla",
-    "antelope",
-    "goat",
-    "rabbit",
+  answers: [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes- definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
   ],
 };
 const app = express();
 
 // configuration
 // middlewares
+
 app.use(logger("dev"));
 app.use(cors());
+app.use(bodyParser.json());
 
 // controllers
 app.get("/hello", (request, response) => {
   response.send("hello");
 });
 
-app.get("/words/random", (request, response) => {
-  const randomIndex = Math.floor(Math.random() * db.words.length);
-  const word = db.words[randomIndex];
-  response.send({ word });
+app.get("/answers/random", (request, response) => {
+  const randomIndex = Math.floor(Math.random() * db.answers.length);
+  const answer = db.answers[randomIndex];
+  response.send({ answer });
 });
+
+app.post("/answers", (request, response) => {
+  console.log(request.body);
+  const { answer } = request.body;
+  if (answer == "" || undefined)
+    return response.status(500).send({ error: "Invalid answer" });
+  db.words.push(answer);
+  response.sendStatus(201);
+});
+
+app.get("/answers", (request, response) => {
+  response.send(db.answers);
+});
+
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000, () => {
   console.log("Server started on http://localhost:3000");
